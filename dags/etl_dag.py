49a1d5847d11b1ad655d5a9bb408bd8413305876
iyzelenko <147039281+iyzelenko@users.noreply.github.com>
2026-05-18 22:05:22 +0000

    final version

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -3,26 +3,18 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import pandas as pd
 
-def load_csv_to_parquet():
-
-    df = pd.read_csv('/workspaces/data-platform/data/students.csv')
-
-    df.to_parquet(
-        '/workspaces/data-platform/data/students.parquet',
-        index=False
-    )
-
-    print("Parquet created")
+def etl():
+    df = pd.read_csv('/opt/airflow/data/students.csv')
+    df.to_parquet('/workspaces/data-platform/data/students.parquet')
 
 with DAG(
     dag_id='etl_pipeline',
-    start_date=datetime(2024,1,1),
-    schedule='@hourly',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='@daily',
     catchup=False
 ) as dag:
 
-    load_task = PythonOperator(
-        task_id='load_csv',
-        python_callable=load_csv_to_parquet,
-        retries=3
+    task = PythonOperator(
+        task_id='run_etl',
+        python_callable=etl
     )
